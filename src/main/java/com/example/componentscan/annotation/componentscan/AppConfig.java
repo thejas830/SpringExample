d195f1f2539e6042cd3c737f19f495b69a1b2d4e
Thejas <54901184+thejas830@users.noreply.github.com>
2026-03-29 00:39:24 +0530

    using annotation rather than xml for bean creation

---
+++ src/main/java/com/example/componentscan/annotation/componentscan/AppConfig.java
@@ -0,0 +1,10 @@
+package com.example.componentscan.annotation.componentscan;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.componentscan.annotation")
+public class AppConfig {
+
+}
