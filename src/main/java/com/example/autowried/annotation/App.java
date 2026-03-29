6e850ccf2711ea5b5d97b526993764bf80761eee
Thejas <54901184+thejas830@users.noreply.github.com>
2026-03-29 10:00:16 +0530

    Using Autowired annotation for injecting bean

---
+++ src/main/java/com/example/autowried/annotation/App.java
@@ -0,0 +1,15 @@
+package com.example.autowried.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args){
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Employee emp = context.getBean("employee", Employee.class);
+        System.out.println(emp);
+
+        Manager manager = context.getBean("manager",Manager.class);
+        System.out.println(manager);
+    }
+}
